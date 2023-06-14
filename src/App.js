7d4d131ba96159f42de4e939116a6101f6dafeda
MariaMagnoliaHernandezRomero <magnolia.magnu@gmail.com>
2023-06-14 21:52:06 +0200

    nuevos arreglos

--- src/App.js
+++ src/App.js
@@ -29,17 +29,31 @@ function App(){
       </nav>
       <main className="lg:pl-28 grid grid-cols-1 lg:grid-cols-8">
         <div className="lg:col-span-6 ">
+          {/*Header... */}
           <header className="p-4">
+             {/*titulo en Buscador... */}
+            <div className="flex flex-col gap-4 mb-6">
           <div>
-            <h1 className="text-2xl">Juan perez Vintage</h1>
-            <p>15-05-2023</p>
+            <h1 className="text-2xl text-black-300">Juan perez Vintage</h1>
+            <p className="text-black-500">15-05-2023</p>
             </div>
             <form>
-              <div>
-                <FiSearch />
-              <input type="text" className="bg-white" />
+              <div className="w-full relative">
+                <FiSearch className="bg-[#A4A4B5] absolute left-3 top-1/2 -translate-y-1/2 text-gray-300"/>
+              <input type="text" className="bg-[#010115] w-full py-2 pl-10 pr-4 text-gray-300 rounded-lgb 
+              outline-none" placeholder="Buscar" />
               </div>
             </form>
+            </div>
+             {/*pestañas... */}
+             <nav className="text-black-300 flex items-center justify-between border-b">
+              <a href="#" className="relative py-2 pr-4 before:w-1/2 before:h-[2px] before:absolute before:bg-[#FD0426] before:left-0 before:rounded-full
+               before:-bottom-[1px] text-[#FD0426] ">Home</a>
+              <a href="#" className="py-2 pr-4">Mujer</a>
+              <a href="#" className="py-2 pr-4">Hombre</a>
+              <a href="#" className="py-2 pr-4">Niño</a>
+              <a href="#" className="py-2 pr-4">tu cuenta</a>
+             </nav>
             </header>
             </div>
         <div className="lg:col-span-2  fixed lg:static right-0">carrito</div>
